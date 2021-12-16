5aebcc0b93430d438f85f8a70a9bb9ccea38d4ec
axi92 <axi92@users.noreply.github.com>
2021-12-16 09:11:30 +0100

    Update lost island obelisks

--- views/js/static/lostisland.js
+++ views/js/static/lostisland.js
@@ -3,6 +3,6 @@ var bounds = [
   [-5, -5],
   [105, 105]
 ];
-var blueObelisk = [0, 30, 'Blue obelisk'];
-var greenObelisk = [30, 0, 'Green obelisk'];
-var redObelisk = [30, 30, 'Red obelisk'];
+var blueObelisk = [25.2, 34.4, 'Blue obelisk'];
+var greenObelisk = [62.7, 56.9, 'Green obelisk'];
+var redObelisk = [28.6, 63.8, 'Red obelisk'];

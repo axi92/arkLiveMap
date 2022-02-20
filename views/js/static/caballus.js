0aa7bfae85c648e7e1c24244090fbdcac38236ee
axi92 <phips_92@yahoo.de>
2022-02-20 14:44:45 +0100

    implement caballus

---
+++ views/js/static/caballus.js
@@ -0,0 +1,8 @@
+var mapfile = '/images/caballus.jpg';
+var bounds = [
+  [-5, -5],
+  [105, 105]
+];
+var blueObelisk = [18.8, 17.7, 'Blue obelisk']; // blau2 39.2, 49.3
+var greenObelisk = [67.3, 37.0, 'Green obelisk'];
+var redObelisk = [45.0, 88.0, 'Red obelisk'];

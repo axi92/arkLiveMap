1cee9b4d298e63334864999e7776402c14c348b8
axi92 <axi92@users.noreply.github.com>
2023-12-23 16:43:57 +0100

    add svartalheim

---
+++ views/js/static/svartalheim.js
@@ -0,0 +1,8 @@
+var mapfile = '/images/svartalheim.jpg';
+var bounds = [
+  [-5, -5],
+  [105, 105]
+];
+var blueObelisk = [11, 18.9, 'Blue obelisk'];
+var greenObelisk = [50.2, 77.8, 'Green obelisk'];
+var redObelisk = [77.9, 18.6, 'Red obelisk'];

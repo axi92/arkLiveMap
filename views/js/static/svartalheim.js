d98031a0e969c8c3ba687a852d605207ea02a5eb
axi92 <axi92@users.noreply.github.com>
2023-12-23 17:45:33 +0100

    add obelisk data for svartalheim

--- views/js/static/svartalheim.js
+++ views/js/static/svartalheim.js
@@ -3,6 +3,6 @@ var bounds = [
   [-0.3, -1.5],
   [101.5, 99.3]
 ];
-// var blueObelisk = [20, 20, 'Blue obelisk'];
-// var greenObelisk = [50.2, 77.8, 'Green obelisk'];
-// var redObelisk = [80.0, 80.0, 'Red obelisk'];
+var blueObelisk = [50.9, 63.4, 'Blue obelisk'];
+var greenObelisk = [34, 40.8, 'Green obelisk'];
+var redObelisk = [45.6, 28.5, 'Red obelisk'];

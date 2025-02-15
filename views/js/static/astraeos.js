e2867da83797089301ff0230a03d7a2fce64e2d4
axi92 <axi92@users.noreply.github.com>
2025-02-15 14:56:58 +0100

    feat(map): add astraeos

---
+++ views/js/static/astraeos.js
@@ -0,0 +1,8 @@
+var mapfile = '/images/astraeos.jpg';
+var bounds = [
+  [-5, -5],
+  [105, 105]
+];
+// var blueObelisk = [18.9, 16.1, 'Blue obelisk'];
+// var greenObelisk = [22.5, 77.8, 'Green obelisk'];
+// var redObelisk = [80.8, 20.3, 'Red obelisk'];

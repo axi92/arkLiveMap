1594eff9d09fbb7c32d1a683f2306d34a3525eae
axi92 <axi92@users.noreply.github.com>
2025-04-24 15:20:49 +0000

    feat: forglar
    Only 3 obelisks are supported, sry

---
+++ views/js/static/forglar.js
@@ -0,0 +1,8 @@
+var mapfile = '/images/forglar.jpg';
+var bounds = [
+  [-5, -5],
+  [105, 105]
+];
+var blueObelisk = [7.4, 14.5, 'Blue obelisk'];
+var greenObelisk = [74.2, 61.6, 'Green obelisk'];
+var redObelisk = [26.8, 52.2, 'Red obelisk'];

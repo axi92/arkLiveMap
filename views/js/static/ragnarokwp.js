7c9ff52660950ebfbd76bc901aaa3664d990509d
axi92 <axi92@users.noreply.github.com>
2025-07-14 22:40:36 +0200

    feat: add new ragnarok_wp image

---
+++ views/js/static/ragnarokwp.js
@@ -0,0 +1,8 @@
+var mapfile = '/images/Ragnarok_WP.jpg';
+var bounds = [
+  [-5, -5],
+  [105, 105]
+];
+var blueObelisk = [18.1, 17.3, 'Blue obelisk'];
+var greenObelisk = [57.0, 38.01, 'Green obelisk'];
+var redObelisk = [35.0, 85.7, 'Red obelisk'];
